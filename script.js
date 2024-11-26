5614a070b43d9c5b8a10324333a0ba8cc0af38f8
the-bug-lover <aleksander24sfj@gmail.com>
2024-11-26 11:17:17 +0100

    added japanese version of names and release date

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const movies = 'https://ghibliapi.vercel.app/films';
+const movies = "https://ghibliapi.vercel.app/films";
 
 const response = await fetch(movies);
 const data = await response.json();
@@ -6,9 +6,19 @@ const data = await response.json();
 console.log(data);
 // console.log("Test");
 
-let list = document.getElementById('ghibliList');
-data.forEach(movie => {
-    let entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(movie.title));
-    list.appendChild(entry);
+let list = document.getElementById("ghibliList");
+data.forEach((movie) => {
+  let entry = document.createElement("li");
+  let jp = document.createElement("span");
+  let en = document.createElement("span");
+
+  jp.appendChild(document.createTextNode(`${movie.original_title} (${movie.release_date})`));
+  jp.id = "jp";
+  en.appendChild(document.createTextNode(`${movie.title} (${movie.release_date})`));
+  en.id = "en";
+
+  entry.appendChild(jp);
+  entry.appendChild(en);
+  entry.id = "movie-title";
+  list.appendChild(entry);
 });
